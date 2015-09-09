18c2a2e021961ad231d002e826ce41cf3e7c8cbb
Daniel Petti <djpetti@gmail.com>
2015-09-09 18:23:16 -0400

    Add signin rollovers to lite plan.

--- tests/test_cron.py
+++ tests/test_cron.py
@@ -9,7 +9,9 @@ import webtest
 
 from google.appengine.ext import testbed
 
+from config import Config
 from membership import Membership
+from plans import Plan
 import cron
 
 
@@ -31,7 +33,7 @@ class ResetSigninHandlerTest(unittest.TestCase):
 
   """ Tests that the cron job restores users properly. """
   def test_user_restore(self):
-    self.user.signins = 10
+    self.user.signins = Config().LITE_VISITS + 2
     self.user.status = "no_visits"
     self.user.put()
 
@@ -41,3 +43,23 @@ class ResetSigninHandlerTest(unittest.TestCase):
     user = Membership.get_by_email("ttesterson@gmail.com")
     self.assertEqual(0, user.signins)
     self.assertEqual("active", user.status)
+
+  """ Tests that unused signins rollover properly. """
+  def test_rollover(self):
+    self.user.signins = Config().LITE_VISITS - 2
+    self.user.status = "active"
+    self.user.put()
+
+    response = self.test_app.get("/cron/reset_signins")
+    self.assertEqual(200, response.status_int)
+
+    user = Membership.get_by_email("ttesterson@gmail.com")
+    self.assertEqual(-2, user.signins)
+    self.assertEqual("active", user.status)
+
+    # Test that signins_remaining gives us the right number.
+    test_plan = Plan("test_lite", 1, 100, "A test plan",
+                     signin_limit = Config().LITE_VISITS)
+    user.plan = "test_lite"
+    remaining = Plan.signins_remaining(user)
+    self.assertEqual(Config().LITE_VISITS + 2, remaining)
